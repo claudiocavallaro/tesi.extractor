5fdcaf247a9c6f1267a17c2b03bf5113896b2437
lords91 <claudio.cavallaro91@gmail.com>
2019-09-13 09:55:58 +0200

    .gitignore fix

--- src/main/java/extractor/Costanti.java
+++
@@ -1,8 +0,0 @@
-package extractor;
-
-public class Costanti {
-
-    public static final String clientId = "143e6b65125042178bcf14796211d5d2";
-    public static final String clientSecret = "c39a44105867434396c5dbecbce77a8e";
-
-}
